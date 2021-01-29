3b6a7e7f7094e850e75165ee8f3ff467962b8d29
Rui Zhao <rui.zhao@linksfoundation.com>
2021-01-29 17:18:23 +0100

    update ci

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ cd eu.brain.iot.ros.edge.node
 export MAVEN_CLI_OPTS='-s ./.m2/settings.xml -B'
 echo $MAVEN_CLI_OPTS
 
-#cat ~/.m2/settings.xml
 
 #cp .m2/settings.xml ~/.m2
 
 mvn $MAVEN_CLI_OPTS initialize
 mvn $MAVEN_CLI_OPTS verify
-#mvn $MAVEN_CLI_OPTS $MAVEN_DEPLOY_OPTS clean package org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy
+mvn $MAVEN_CLI_OPTS $MAVEN_DEPLOY_OPTS clean package org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy
 
 #new-UC
